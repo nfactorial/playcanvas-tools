22b504375a2023780efd39bf24ee4026d8c242d9
nfactorial <nfactorial@btopenworld.com>
2019-08-22 09:49:20 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const https = require('https');
+const Job = require('./lib/job');
+
+const Tools = module.exports = {};
+
+/**
+ * Downloads the specified project from the play-canvas server.
+ * @param {string} targetPath - Path and name where the downloaded zip file will be stored.
+ * @param {number} projectId - Identifier of the project to be downloaded.
+ * @param {string} projectName - Name of the project to be downloaded.
+ * @param {string} accessToken - Organization access token allowing access rights for the download operation.
+ * @param {number[]} scenes - List of scenes to be downloaded included within the downloaded package.
+ * @returns {Promise<string>}
+ */
+Tools.download = async function(targetPath, projectId, projectName, accessToken, scenes) {
+    if (!targetPath || typeof targetPath !== 'string')
+        throw new Error('A valid target path must be supplied');
+
+    if (typeof projectId !== 'number')
+        throw new Error('Invalid project id, must be a numeric value');
+
+    const jobId = await Job.createDownload(https, projectId, projectName, accessToken, scenes);
+
+    return Job.wait(https, accessToken, jobId);
+};
---
+++ readme.md
@@ -0,0 +1,8 @@
+# PlayCanvas Tools
+This package provides scripting support tools for interacting with the playcanvas server.
+
+## Installation
+Install the package using npm with:
+```javascript
+npm install --save @nfactorial/playcanvas-tools
+```
